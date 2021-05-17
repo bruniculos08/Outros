c557ddfa20516d7154cd7ead7c4f441b4ec9fcf7
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-17 15:43:05 -0300

    Create atoi.c

---
+++ Harvard CS50/pset1/atoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, string argv[]) //remeber to check each individual character to see if it's a integer
+{char s = argv[1];
+ int x = atoi (&s);
+ int n = strlen(&argv[1]);
+ int y = n+x;
+    //if (x>=48 && x<=57)
+   {
+   printf(" x=%i, n=%i, n+x=%i \n", x, n, y);
+   }
+    //else
+    //{
+    //  printf("ERROR \n");
+   // }
+}
