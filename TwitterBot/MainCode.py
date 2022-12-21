180dc35d872025e3939f6f297db96a62bd7e8499
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2022-12-21 16:03:16 -0300

    update

--- TwitterBot/MainCode.py
+++ TwitterBot/MainCode.py
@@ -32,13 +32,22 @@ def semestreUdesc():
     semesters_file = open(generalPath + "\\semestres.txt")
     lines = semesters_file.readlines()
 
-    with open(generalPath + "\\ultimoPostData.txt") as f:
-        last_post = f.readline()
+    actual_case = lines[0].split(",")
+    print(actual_case)
 
-    print(datetime.now() < datetime.strptime(last_post, "%Y-%m-%d %H:%M:%S"))
-    print(datetime.now())
+    last_post_file = open(generalPath + "\\ultimoPostData.txt")
+    last_post = last_post_file.readline()
 
+    actual_date = datetime.now().date()
+    last_post_date = datetime.strptime(last_post, "%Y-%m-%d").date()
 
+    if(actual_date > last_post_date):
+        if(actual_case[0] == "fim"):
+            result = datetime.strptime(actual_case[2], "%Y-%m-%d").date() - actual_date
+            print(f"Faltam {int(result.days/30)} meses e {result.days%30} dias para o fim do semestre na UDESC.")
+
+    # print(datetime.now().date() <= datetime.strptime(last_post, "%Y-%m-%d").date())
+    # print(datetime.today().date())
 
     # last_info = lines[0].split(",")
 
